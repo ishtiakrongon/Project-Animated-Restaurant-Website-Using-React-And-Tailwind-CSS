5d0740d6d937e87003a91b4491ad0aeb5bd8f028
Md. Ishtiak Ahmed <ishtiak.rongon@gmail.com>
2024-08-06 20:21:31 +0600

    responsive dropdown updated

--- my-project/src/components/Hero/Hero.jsx
+++ my-project/src/components/Hero/Hero.jsx
@@ -4,26 +4,88 @@ import Spoon from "../../assets/food/spoon.png";
 import Banana from "../../assets/food/banana2.png";
 import Leaf from "../../assets/food/leaf.png";
 import { IoCartOutline } from 'react-icons/io5';
+import { motion } from 'framer-motion';
+
+
+const SlideUp = (delay) => {
+    return {
+        hidden: {
+            y: "100%",
+            opacity: "0"
+        },
+        show: {
+            y: 0,
+            opacity: 1,
+            transition: {
+                duration: 0.6,
+                delay: delay
+            }
+        },
+    }
+}
 
 const Hero = () => {
   return (
     <main>
-        <div className="container min-h-[600px] flex justify-center">
+        <div className="container min-h-[600px] flex justify-center relative z-10">
             <div className="grid grid-cols-1 md:grid-cols-2 gap-12 lg:gap-24 place-items-center justify-between">
                 {/* text content here */}
                 <div className='space-y-3 mt-14 text-center md:text-left md:mt-0'>
-                    <h1 className="relative text-5xl lg:text-7xl xl:text-8xl font-bold uppercase text-outline text-transparent">
+                    <motion.h1 variants={SlideUp(0.5)}
+                    initial="hidden"
+                    whileInView="show"
+                     className="relative text-5xl lg:text-7xl xl:text-8xl font-bold uppercase text-outline text-transparent">
                         Yummy
                         <img src={Leaf} alt="Ymmuy logo" className="absolute w-[50px] top-0 right-0 md:right-[100px]"/>
-                    </h1>
-                    <h2 className="text-5xl lg:text-7xl xl:text-8xl font-bold uppercase">BREAKFAST</h2>
-                    <p className='text-sm'>
+                    </motion.h1>
+                    <motion.h2 variants={SlideUp(1)}
+                    initial="hidden"
+                    whileInView="show"
+                    className="text-5xl lg:text-7xl xl:text-8xl font-bold uppercase">BREAKFAST</motion.h2>
+                    <motion.p variants={SlideUp(1.5)}
+                    initial="hidden"
+                    whileInView="show"
+                    className='text-sm'>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, in magni nemo voluptatibus doloribus rem veniam possimus quibusdam ex. Laborum dolores repudiandae sint a enim fugiat assumenda veniam accusamus animi?
-                    </p>
+                    </motion.p>
+                    <motion.button
+                    variants={SlideUp(2)}
+                    initial="hidden"
+                    whileInView="show"
+                    className='btn-primary inline-block !mt-10'>
+                        <IoCartOutline className='inline mr-2' />
+                        Order Now
+                    </motion.button>
                 </div>
                 {/* images content here */}
+                <div className='relative'>
+                    <motion.img
+                    initial={ {opacity: 0, rotate: 20, x:200, y:100} }
+                    whileInView={ {opacity: 1, rotate: 0, x: 0, y: 0} }
+                    transition={ {duration: 0.8} }
+                    src={FoodPng} alt="" className='w-[450px] img-shadow' />
+                    <motion.img
+                    initial={ {opacity: 0, rotate: 20, x:200, y:100} }
+                    whileInView={ {opacity: 1, rotate: 0, x: 0, y: 0} }
+                    transition={ {duration: 0.8} }
+                    src={Spoon} alt="" className='w-[350px] absolute bottom-[-120px] -left-16 rotate-[75deg] img-shadow' />
+                    <motion.img 
+                    initial={ {opacity: 0, rotate: 20, x:200, y:100} }
+                    whileInView={ {opacity: 1, rotate: 0, x: 0, y: 0} }
+                    transition={ {duration: 0.8} }
+                    src={Banana} alt="" className='w-[400px] absolute top-[-30px] right-[-130px] md:right-[-160px] img-shadow' />
+                </div>
             </div>
         </div>
+
+    {/* background yellow */}
+    <motion.div 
+    initial={ {opacity: 0, rotate: 60, x: 200, y: 100} }
+    whileInView={ {opacity: 1, rotate: 40, x:0, y:0} }
+    className='w-[2500px] h-[2500px] rounded-3xl bg-lightYellow absolute top-[-30%] left-[70%] z-0'>
+
+    </motion.div>
+
     </main>
   )
 }
